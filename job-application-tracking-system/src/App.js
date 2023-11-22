052121ed52d5685e6cd86a9c0ff3c05578c188b8
Saravanan-07 <106005647+Saravanan-07@users.noreply.github.com>
2023-11-22 11:28:40 +0530

    Update App.js

--- job-application-tracking-system/src/App.js
+++ job-application-tracking-system/src/App.js
@@ -106,18 +106,3 @@ function App() {
 }
 
 export default App;
-
-/*import React from 'react';
-import './App.css';
-import TodoList from './components/TodoList';
-
-function App() {
-  return (
-    <div className='todo-app'>
-      <TodoList />
-    </div>
-  );
-}
-
-export default App;*/
-
